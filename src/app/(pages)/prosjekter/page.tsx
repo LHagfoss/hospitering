9cef4e588509401e0e96446e09d21dcbdd40b118
LHagfoss <afs0028447@agderskolen.no>
2024-12-01 22:12:33 +0100

    balls

--- src/app/(pages)/prosjekter/page.tsx
+++ src/app/(pages)/prosjekter/page.tsx
@@ -14,16 +14,16 @@ export default function Home() {
           <h2 className="text-5xl">på IT VG2</h2>
         </div>
       </div>
-      <div className="container mx-auto p-10 px-44">
+      <div className="container mx-auto p-10 px-52">
           <h1 className="text-7xl">Nettsider</h1>
       </div>
-      <div className="container mx-auto p-10 px-44">
+      <div className="container mx-auto p-10 px-52">
       <h1>Vi lager mange forskjellige nettsider med skolen. Vi har for eksempel laget et lite spill av tic tac toe i react. Vi har også hatt egne prosjekter med nettsider som portoføljer og andre gøye nettsider. Dette skal da hjelpe oss til når vi kommer ut til det ekte markedet.</h1>
       </div>
       <div className="w-[100vw] h-[10vh] flex justify-center items-center overflow-hidden font-[--font-geist-mono]">
           <h1 className="text-7xl">Spill og Programvarer</h1>
       </div>
-      <div className="container mx-auto p-10 px-44">
+      <div className="container mx-auto p-10 px-52">
       <h1>Vi lager mange forskjellige Programvarer med skolen. Noen av oss har for eksempel laget et spill som heter city case som er laget i unity. Vi har også hatt egne prosjekter med andre spill og programvarer. Noen av oss har til og med greid å lage et virus. Dette skal da hjelpe oss til når vi kommer ut til det ekte markedet.</h1>
       <Image className= "rounded-3xl" src={sc1} alt="yoyo"/>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,19 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
+      <Navbar />
       <div className="w-[100vw] h-[100vh] flex justify-center items-center overflow-hidden font-[--font-geist-mono]">
-        <Navbar />
         <div className="flex flex-col">
           <h1 className="text-9xl">Utvilking</h1>
-          <h2 className="text-5xl">på IT VG2</h2>
+          <h2 className="text-5xl">IT VG2 Tangen</h2>
+          <p className="">Laget av Lucas og Odin 🤍🤍🤍</p>
+        </div>
+      </div>
+      <div className="w-[100vw] h-[100vh]">
+        <div className="container mx-auto p-10 px-52">
+          <div className="w-full h-[50vh] rounded-3xl bg-zinc-900">
+
+          </div>
         </div>
       </div>
       <Footer />
